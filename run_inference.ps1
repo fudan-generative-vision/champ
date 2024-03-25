66ac8ea2e7cf783b1835b995fbdb27e4f6b09bea
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2024-03-25 22:20:31 +0800

    init

--- run_inference.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-.\venv\Scripts\activate
-
-$Env:HF_HOME = "./huggingface"
-$Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
-$Env:PYTHONPATH = $PSScriptRoot
-$Env:HF_ENDPOINT = "https://hf-mirror.com"
-
-python.exe "inference.py" --config configs/inference.yaml
