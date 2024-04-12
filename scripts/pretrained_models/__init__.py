625afac23e2c36f3bc4a2ff943fa87d5f4969c8c
AricGamma <leeway213@outlook.com>
2024-04-12 21:19:37 +0800

    fix: optimize model dependencies downloads scripts (#67)
    Co-authored-by: leeway.zlw <leeway.zlw@outlook.com>

--- scripts/pretrained_models/__init__.py
+++ scripts/pretrained_models/__init__.py
@@ -2,13 +2,15 @@ import os
 from pathlib import Path
 from hmr2.models import CACHE_DIR_4DHUMANS
 
+PROJECT_ROOT = Path(__file__).parent.parent.parent;
+
+PRETRAINED_MODEL_DIR = f"{PROJECT_ROOT}/pretrained_models";
+
 HMR2_MODELS_DIR = CACHE_DIR_4DHUMANS
 HMR2_DEFAULT_CKPT = f"{HMR2_MODELS_DIR}/logs/train/multiruns/hmr2/0/checkpoints/epoch=35-step=1000000.ckpt"
 
 SMPL_MODEL_DIR = f"{CACHE_DIR_4DHUMANS}/data/smpl"
 SMPL_MODEL_PATH = f"{SMPL_MODEL_DIR}/SMPL_NEUTRAL.pkl"
 
-DETECTRON2_MODEL_DIR = os.path.join(
-    Path(__file__).parent.parent.parent
-)
+DETECTRON2_MODEL_DIR = f"{PRETRAINED_MODEL_DIR}/detectron2"
 DETECTRON2_MODEL_PATH = f"{DETECTRON2_MODEL_DIR}/model_final_f05665.pkl"
